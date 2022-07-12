4dbd83d0feec93d0ff95d75113176d5fa8d27f1c
jm-taotao <aaa7410aaa>
2022-07-13 00:14:10 +0800

    jyt

--- src/request/api.js
+++
@@ -1,7 +0,0 @@
-
-//登录
-export const login = '/api/web/user/login';
-//获取菜单信息
-export const getMenuList = p => get('/api/web/menu/tree',p);
-//获取用户分页列表
-export const getPageUserList = p => get('/api/web/user/list',p);
